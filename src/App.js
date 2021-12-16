2d75595a68283df339152787df63cab92835e4e4
fatkurfaris <@fatkurrizal12@gmail.com>
2021-12-16 16:47:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomeDefault/HomeDefault.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavbarHome from "../../components/NavbarHome/NavbarHome";
+
+export default function HomeDefault() {
+    return (
+        <>
+            <NavbarHome />
+        </>
+    )
+}

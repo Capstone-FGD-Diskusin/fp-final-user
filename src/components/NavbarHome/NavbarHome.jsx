91f255ad83ec670b34d84fa677b4d724a2c9deaf
fatkurfaris <@fatkurrizal12@gmail.com>
2021-12-16 17:01:46 +0700

    homepage

--- src/components/NavbarHome/NavbarHome.jsx
+++ src/components/NavbarHome/NavbarHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
-export default funciton NavbarHome{
+export default function NavbarHome() {
     return (
         <>
         </>
